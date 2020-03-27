0a18171ecb3569241babe00d38d8e73027971b07
Damon <shunshunhan@gmail.com>
2020-03-27 09:58:41 +0800

    Update MainApplication.java
    //111

--- order-service-comsumer/src/main/java/com/hss/gmall/service/impl/MainApplication.java
+++ order-service-comsumer/src/main/java/com/hss/gmall/service/impl/MainApplication.java
@@ -6,6 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.hss.gmall.service.OrderService;
 
+//111
 public class MainApplication {
 
 	public static void main(String[] args) throws IOException {
